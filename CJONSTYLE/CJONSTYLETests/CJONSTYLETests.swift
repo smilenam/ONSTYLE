8b12366e8eddcefed520cc579a093a2b7daef823
smiles1080@naver.com <>
2025-08-23 11:38:07 +0900

    프로젝트 생성

---
+++ CJONSTYLE/CJONSTYLETests/CJONSTYLETests.swift
@@ -0,0 +1,17 @@
+//
+//  CJONSTYLETests.swift
+//  CJONSTYLETests
+//
+//  Created by 남태현 on 8/23/25.
+//
+
+import Testing
+@testable import CJONSTYLE
+
+struct CJONSTYLETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
